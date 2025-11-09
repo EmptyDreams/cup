c7226c3a16aabaffd7a4669fcf54fc34d696d42b
kmar <minedreams@qq.com>
2025-11-09 20:39:45 +0800

    update minijava

--- testgrammars/minijava/make.sh
+++ testgrammars/minijava/make.sh
@@ -6,9 +6,7 @@ java -jar ../../target/dist/java-cup-11b.jar \
   -parser MiniJavaParser \
   -symbols MiniJavaSymbols \
   -nonterms \
-  -xmlactions \
   -destdir ./java \
   -ast Node%s \
   minijava.cup
 java -cp ../../target/dist/java-cup-11b-runtime.jar:. ./java/classes/MiniJavaParser simple.minijava simple.xml /
-basex codegen.sq output.xml > simple.minijvm
