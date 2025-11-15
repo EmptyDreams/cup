d9fdda605da06820a4f4af495a1fb00c3df8ad92
kmar <minedreams@qq.com>
2025-11-15 21:25:10 +0800

    test: update calculator grammar and scanner

--- testgrammars/calc/scanner.java
+++ testgrammars/calc/scanner.java
@@ -1,57 +1,90 @@
 import java_cup.runtime.*;
+import java_cup.runtime.symbol.complex.ComplexSymbolFactory;
+import static java_cup.runtime.symbol.complex.ComplexLocation.NO_LOCATION;
 
 class Main {
-    public static void main(String[] argv) throws Exception{
-	System.out.println("Please type your arithmethic expression:");
-	Parser p = new Parser(new scanner());
-	p.parse();
+
+    public static void main(String[] argv) throws Exception {
+        System.out.println("Please type your arithmethic expression:");
+        SymbolFactory sf = new ComplexSymbolFactory(CalcSymbols.TERMINAL_NAMES, null);
+        CalcParser p = new CalcParser(new scanner(sf), sf);
+        p.parse();
     }
+
 }
 
-public class scanner {
+public class scanner implements Scanner {
+
+    private SymbolFactory sf;
+
+    public scanner(SymbolFactory sf) {
+        this.sf = sf;
+    }
+
     /* single lookahead character */
     protected static int next_char;
 
-    /* we use a SymbolFactory to generate Symbols */
-    private SymbolFactory sf = new DefaultSymbolFactory();
-
     /* advance input by one character */
-    protected void advance() throws java.io.IOException
-    { next_char = System.in.read(); }
+    protected void advance() throws java.io.IOException {
+        next_char = System.in.read();
+    }
 
     /* initialize the scanner */
-    public void init() throws java.io.IOException
-    { advance(); }
+    public void init() throws java.io.IOException {
+        advance();
+    }
 
     /* recognize and return the next complete token */
-    public Symbol next_token() throws java.io.IOException
-    {
-	for (;;)
-	    switch (next_char)
-		{
-		case '0': case '1': case '2': case '3': case '4':
-		case '5': case '6': case '7': case '8': case '9':
-		    /* parse a decimal integer */
-		    int i_val = 0;
-		    do {
-			i_val = i_val * 10 + (next_char - '0');
-			advance();
-		    } while (next_char >= '0' && next_char <= '9');
-		    return sf.newSymbol("NUMBER",sym.NUMBER, new Integer(i_val));
+    @Override
+    public Symbol next_token() throws java.io.IOException {
+        for (; ; ) {
+            switch (next_char) {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    /* parse a decimal integer */
+                    int i_val = 0;
+                    do {
+                        i_val = i_val * 10 + (next_char - '0');
+                        advance();
+                    } while (next_char >= '0' && next_char <= '9');
+                    return sf.newSymbol(CalcSymbols.NUMBER, NO_LOCATION, i_val);
 
-		case ';': advance(); return sf.newSymbol("SEMI",sym.SEMI);
-		case '+': advance(); return sf.newSymbol("PLUS",sym.PLUS);
-		case '-': advance(); return sf.newSymbol("MINUS",sym.MINUS);
-		case '*': advance(); return sf.newSymbol("TIMES",sym.TIMES);
-		case '(': advance(); return sf.newSymbol("LPAREN",sym.LPAREN);
-		case ')': advance(); return sf.newSymbol("RPAREN",sym.RPAREN);
+                case ';':
+                    advance();
+                    return sf.newSymbol(CalcSymbols.SEMI, NO_LOCATION);
+                case '+':
+                    advance();
+                    return sf.newSymbol(CalcSymbols.PLUS, NO_LOCATION);
+                case '-':
+                    advance();
+                    return sf.newSymbol(CalcSymbols.MINUS, NO_LOCATION);
+                case '*':
+                    advance();
+                    return sf.newSymbol(CalcSymbols.TIMES, NO_LOCATION);
+                case '(':
+                    advance();
+                    return sf.newSymbol(CalcSymbols.LPAREN, NO_LOCATION);
+                case ')':
+                    advance();
+                    return sf.newSymbol(CalcSymbols.RPAREN, NO_LOCATION);
 
-		case -1: return sf.newSymbol("EOF",sym.EOF);
+                case -1:
+                    return sf.newSymbol(CalcSymbols.EOF, NO_LOCATION);
 
-		default:
-		    /* in this simple scanner we just ignore everything else */
-		    advance();
-		    break;
-		}
+                default:
+                    /* in this simple scanner we just ignore everything else */
+                    advance();
+                    break;
+            }
+        }
     }
+
 };
